4e0e234abf0c3918734019770c7b3a9c213f1faa
Crlwebby <1303965943@qq.com>
2021-01-21 10:33:07 +0800

    better perfect_num algorithm

--- crlwebby_code/day5/perfect_num.py
+++ crlwebby_code/day5/perfect_num.py
@@ -1,7 +1,9 @@
 for i in range(1,10000):
     sum = 0
-    for j in range(1,i):
+    for j in range(1,int(i ** 0.5) + 1):
         if i%j == 0:
             sum +=j
+            if j > 1 and i != j * j:
+                sum += i // j
     if sum == i:
         print(sum)
